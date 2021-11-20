4789b281ca7812a53a47304f552049add1717997
xiaolu <1324790234@qq.com>
2021-11-20 17:31:57 +0800

    流的操作新方法takeWhile、dropWhile

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -53,6 +54,32 @@ public class Main {
         Map<String, Integer> map = Map.of("1", 1, "2", 2);
         System.out.println(map.get("1"));
         //map.put("3", 3);// java.lang.UnsupportedOperationException
+
+    }
+
+    /**
+     * 流的操作
+     */
+    @Test
+    public void testStream() {
+        var list = List.of(1, 2, 3, 4);
+        // 因为println方法和forEach方法都是有参数，无返回值，所以可以用：：这种
+        list.stream().forEach(x -> System.out.print(x % 2 ));
+
+        // java.lang.NullPointerException
+        //Stream<Object> stream = Stream.of(null);
+        // ofNullable会调 empty()创建空数组
+        Stream<Object> stream = Stream.ofNullable(null);
+        stream.forEach(System.out::println);
+
+        System.out.println("1、========");
+        Stream<Integer> stream1 = Stream.of(1, 2, 3, 4, 5, 6);
+        // takewhile()方法接受所有值，直到谓词返回false为止
+        stream1.takeWhile(x -> x % 2 != 0).forEach(System.out::print);//1
+        System.out.println("\n2、========");
+        Stream<Integer> stream2 = Stream.of(1, 2, 3, 4, 5, 6);
+        // dropWhile()方法丢弃所有值，直到谓词返回false为止
+        stream2.dropWhile(x -> x % 2 != 0).forEach(System.out::print);//23456
 
     }
 }
